54a258d734307fc455396e2510fdc2d8f33947f2
Charles Loomis <cal@sixsq.com>
2014-11-20 12:08:41 +0100

    refactor pull script to allow multiple repository URLs to be tried

--- git-pull.sh
+++ git-pull.sh
@@ -1,22 +1,67 @@
 #!/bin/bash
 
-update_ss_repo(){
-  if [ -d ${1} ]
-  then
-    echo "Updating ${1}..."
-    (cd ${1}; git pull)
-  else
-    echo "Repo ${1} is not yet cloned here."
-    echo "Cloning ${1}..."
-    git clone git@github.com:slipstream/${1}.git
-  fi
-  echo
+GITHUB_URLS=(
+"git@github.com:slipstream"
+"https://github.com/slipstream"
+)
+
+REPOS=(
+"SlipStreamDocumentation"
+"SlipStreamDocumentationAPI"
+"SlipStreamUI"
+"SlipStreamServer"
+"SlipStreamServerDeps"
+"SlipStreamClient"
+"SlipStreamConnectors"
+)
+
+# loop over possible repository locations for cloning
+clone_ss_repo(){
+  repo=${1}
+
+  for i in "${GITHUB_URLS[@]}"
+  do
+      repo_url=${i}/${repo}.git
+      echo "Cloning ${repo_url}"
+      git clone ${repo_url}
+      rc=$?
+      if [ ${rc} -eq 0 ]
+      then
+          break;
+      else
+          echo "Cloning ${repo_url} failed!"
+      fi
+  done
+
+  return ${rc}
 }
 
-update_ss_repo "SlipStreamDocumentation"
-update_ss_repo "SlipStreamDocumentationAPI"
-update_ss_repo "SlipStreamUI"
-update_ss_repo "SlipStreamServer"
-update_ss_repo "SlipStreamServerDeps"
-update_ss_repo "SlipStreamClient"
-update_ss_repo "SlipStreamConnectors"
+# Update or clone the given repository.
+update_ss_repo(){
+  repo=${1}
+
+  rc=0
+  if [ -d ${repo} ]
+  then
+    echo "Updating ${repo}..."
+    (cd ${repo}; git pull)
+    rc=$?
+  else
+    echo "Repo ${repo} is not yet cloned here."
+    clone_ss_repo ${repo}
+    rc=$?
+  fi
+
+  if [ ${rc} -ne 0 ]
+  then
+      echo "Clone of ${repo} failed."
+      exit ${rc}
+  fi
+}
+
+# Loop through all requested repositories and update or clone.
+for repo in ${REPOS[@]}
+do
+    update_ss_repo ${repo}
+    echo
+done
