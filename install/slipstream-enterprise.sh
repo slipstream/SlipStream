80b7a97ac97fd4e260d029304bf32b465dc5228f
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2015-04-26 21:52:45 +0200

    Updated lipstream-community.sh slipstream-enterprise.sh to use slipstream-install.sh

--- install/slipstream-enterprise.sh
+++ install/slipstream-enterprise.sh
@@ -2,32 +2,50 @@
 set -e
 set -o pipefail
 
-REPOS=(release candidate snapshot)
+_SCRIPT_NAME=${0##*/}
+
+function usage() {
+    echo -e "usage:\n$_SCRIPT_NAME [repo]
+ - repo: <release|candidate|snapshot> (default: release)"
+    exit 1
+}
+
+while getopts : opt; do
+    case $opt in
+    \?)
+        usage
+        ;;
+    esac
+done
+
+GH_BASE_URL=https://raw.githubusercontent.com/slipstream/SlipStream
 
 declare -A REPO_TO_TAG
 REPO_TO_TAG[snapshot]=master
 REPO_TO_TAG[candidate]=candidate-latest
 REPO_TO_TAG[release]=release-latest
 
-declare -A REPO_TO_YUM
-REPO_TO_YUM[snapshot]=Snapshots
-REPO_TO_YUM[candidate]=Candidates
-REPO_TO_YUM[release]=Releases
+function _check_repo() {
+    if ! test "${REPO_TO_TAG[$1]+isset}"; then
+        usage
+    fi
+}
+
+function _download() {
+    TO=$1
+    shift
+    FROM=$SCRIPT_BASE_URL/$TO
+    echo -n "::: Downlading $@... "
+    curl -sSf -k -o $TO $FROM || { echo "Failed downloading $FROM"; exit 1; }
+    echo "done."
+    chmod +x $TO
+}
 
 REPO=${1:-release}
-if ! test "${REPO_TO_TAG[$REPO]+isset}"; then echo "Please provide one of: ${REPOS[@]}"; exit; fi
+_check_repo $REPO
+SCRIPT_BASE_URL=$GH_BASE_URL/${REPO_TO_TAG[$REPO]}/install
 
-#
-# SlipStream Server.
-#
-echo ":::"
-echo "::: SlipStream Service."
-echo ":::"
-SS_INSTALL_SCRIPT=https://raw.githubusercontent.com/slipstream/SlipStream/${REPO_TO_TAG[$REPO]}/install/slipstream.sh
-echo -n "::: Downlading SlipStream installation script... "
-curl -sSf -k -o slipstream.sh $SS_INSTALL_SCRIPT || { echo "Failed downloading $SS_INSTALL_SCRIPT"; exit 1; }
-echo "done."
-chmod +x slipstream.sh
-
-./slipstream.sh -s ${REPO_TO_YUM[$REPO]}-enterprise
+SCRIPT=slipstream-install.sh
+ _download $SCRIPT "SlipStream installation wrapper script"
+./$SCRIPT enterprise $REPO
 
