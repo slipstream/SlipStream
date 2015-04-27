1fb1ffac01ba9c60a9d52ae43e6871203018726e
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2015-04-27 19:18:38 +0200

    Updated NRPE installation script with better logging.

--- install/ss-install-nrpe.sh
+++ install/ss-install-nrpe.sh
@@ -3,7 +3,9 @@
 
 # Usage: <Nagios IP/hostname>
 
-set -ex
+set -e
+set -o pipefail
+set -o errtrace
 
 # Nagios remote plugin executor
 NAGIOS_SERVER=${1:?"Provide Nagios server hostname/IP."}
@@ -15,6 +17,29 @@ LIB_DIR=/usr/lib64
 _NAGIOS_LIBEXEC=/usr/local/nagios/libexec
 mkdir -p ${_NAGIOS_LIBEXEC}
 CHECK_MEM_BIN=${_NAGIOS_LIBEXEC}/check_mem
+
+LOG_FILE=/tmp/slipstream-nrpe-install.log
+exec 4>&2 3>&1 1>>${LOG_FILE} 2>&1
+
+function _print_on_trap() {
+    _prints "\nERROR! Check log file ${LOG_FILE}\n... snippet ...\n$(tail -5 ${LOG_FILE})"
+}
+
+function _on_trap() {
+    _print_on_trap
+}
+
+trap '_on_trap' ERR
+
+function _prints() {
+    echo -e "$@" 1>&3
+}
+function _print() {
+    _prints "::: $@"
+}
+function _printn() {
+    echo -en "::: $@" 1>&3
+}
 
 function _install_check_mem () {
     # check_mem
@@ -31,24 +56,28 @@ function _iptables_get_accept_rule () {
 }
 
 function add_nrpe_firewall_rules () {
-    echo "Adding NRPE firewall rules..."
+    _printn " adding NRPE firewall rules... "
     NRPE_PORT=5666
     NRPE_RULE="$(_iptables_get_accept_rule $NRPE_PORT)"
 
     grep -q "dport $NRPE_PORT.*ACCEPT" /etc/sysconfig/iptables && \
-        { echo "Port already added."; return 0; } || true
+        { _prints "(NRPE port already added) done."; return 0; } || true
 
     iptables-save > /etc/sysconfig/iptables
     sed -i.bak -e "/-A INPUT -j REJECT/i ${NRPE_RULE}" /etc/sysconfig/iptables
     service iptables restart || \
         { cp /etc/sysconfig/iptables.bak /etc/sysconfig/iptables; service iptables restart; }
+    _prints "done."
 }
 
 function install_nrpe_monitoring () {
+    _printn " installing NRPE... "
+    for pkg in $NRPE_DEPS; do
+        yum -y --enablerepo=epel install $pkg
+    done
+    _prints "done."
 
-    echo "Installing NRPE..."
-    yum -y --enablerepo=epel install $NRPE_DEPS
-
+    _printn " configuring SlipStream NRPE checks... "
     sed -i -e 's|^allowed_hosts=.*$|allowed_hosts='${NAGIOS_SERVER}'|' /etc/nagios/nrpe.cfg
     sed -i -e 's|^debug=.*$|debug=1|' /etc/nagios/nrpe.cfg
     sed -i -e 's|^include_dir=.*$|include_dir='${NRPE_D}'|' /etc/nagios/nrpe.cfg
@@ -62,7 +91,9 @@ command[check_rootpart]=$LIB_DIR/nagios/plugins/check_disk -w 20% -c 10% -p /
 command[check_slipstream_backup]=/opt/slipstream/backup/nagios_check_slipstream_backup_timestamp
 command[check_mem]=${CHECK_MEM_BIN} -w $ARG1$ -c $ARG2$ -f -C
 EOF
+    _prints "done."
 
+    _printn " starting NRPE service... "
     # enable arguments to NRPE commands
     sed -i -e 's/^dont_blame_nrpe=.*/dont_blame_nrpe=1/' /etc/nagios/nrpe.cfg
 
@@ -70,7 +101,11 @@ EOF
     chkconfig --add nrpe
 
     service nrpe restart
+    _prints "done."
 }
 
+_print "Installing SlipStream NRPE monitoring."
 install_nrpe_monitoring
 add_nrpe_firewall_rules
+_print "SlipStream NRPE monitoring installed."
+
