6ee751cab958faba7c7642e85f5e1aab3d9ce63b
Charles Loomis <cal@sixsq.com>
2015-06-08 07:04:29 +0200

    rocci repository file is back for now; switch back to standard installation procedure

--- install/connectors/connector-occi.sh
+++ install/connectors/connector-occi.sh
@@ -5,23 +5,11 @@ function deploy() {
     rpm -ivH http://repository.egi.eu/sw/production/umd/2/sl6/x86_64/updates/umd-release-2.0.0-2.el6.noarch.rpm
 
     #
-    # The link to the ROCCI repository configuration at EGI no longer works.
-    # Instead create a yum.d entry directly from the repository URL.  Note
-    # that the repository URL MUST NOT end with a slash.  The EGI repository
-    # does not do a redirect from the URL with the slash to the one without.
+    # The link to the ROCCI repository at EGI is unstable.  In addition,
+    # the actual RPM repository sometimes returns a 404 error, so working
+    # around the instability of the repository isn't reliable.
     #
-    #curl -o /etc/yum.repos.d/rocci.repo http://repository.egi.eu/community/software/rocci.cli/4.2.x/releases/repofiles/sl-6-x86_64.repo
-    cat > /etc/yum.repos.d/rocci.repo <<EOF
-[rocci-cli]
-name=ROCCI CLI repository at EGI
-baseurl=http://repository.egi.eu/community/software/rocci.cli/4.3.x/releases/sl/6/x86_64/RPMS
-enabled=1
-protect=0
-gpgcheck=0
-autorefresh=1
-type=rpm-md
-
-EOF
+    curl -o /etc/yum.repos.d/rocci.repo http://repository.egi.eu/community/software/rocci.cli/4.2.x/releases/repofiles/sl-6-x86_64.repo
 
     yum -y install slipstream-connector-occi
 }
