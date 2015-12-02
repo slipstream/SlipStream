2f4135625e6ab6ebf4d861c45496b5c279450bdb
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2015-12-02 11:21:20 +0100

    Added service test.

--- install/ss-from-sources.sh
+++ install/ss-from-sources.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash
 
+set -x
 set -e
 
 slipstream_version=`ss-get slipstream_version`
@@ -11,6 +12,34 @@ slipstream_ui_version=`ss-get slipstream_ui_version`
 skip_tests=`ss-get skip_tests`
 install_examples=`ss-get install_examples`
 slipstream_backend=`ss-get slipstream_backend`
+with_refconf=`ss-get with_refconf`
+NEXUS_CREDS=`ss-get nexus_creds`
+REFCONF_NAME=`ss-get refconf_name`
+TEST_SERVICE=`ss-get test_service`
+
+echo "=== Parameters ===
+slipstream_version = $slipstream_version
+slipstream_client_version = $slipstream_client_version
+slipstream_connectors_version = $slipstream_connectors_version
+slipstream_server_version = $slipstream_server_version
+slipstream_server_deps_version = $slipstream_server_deps_version
+slipstream_ui_version = $slipstream_ui_version
+skip_tests = $skip_tests
+install_examples = $install_examples
+slipstream_backend = $slipstream_backend
+with_refconf = $with_refconf
+NEXUS_CREDS = $NEXUS_CREDS
+REFCONF_NAME = $REFCONF_NAME
+=== --- ==="
+
+
+function _is_true() {
+    if [ "x${1}" == "xtrue" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
 #
 # upgrade system
@@ -39,7 +68,7 @@ yum install -y \
   git \
   rpm-build \
   createrepo
-  
+
 #
 # SlipStream python dependencies that require
 # versions that are more recent than packages.
@@ -117,14 +146,59 @@ tar zxf ~/SlipStreamBootstrap/SlipStream/yum/target/SlipStream*.tar.gz
 cd -
 
 #
-# quick installation from local repository
+# installation from local repository
 #
-ss-set statecustom "Installing SlipStream..."
-export SLIPSTREAM_EXAMPLES=${install_examples}
-curl -sSfL -o /tmp/slipstream.sh \
-    https://raw.githubusercontent.com/slipstream/SlipStream/master/install/slipstream.sh
-chmod +x /tmp/slipstream.sh
-/tmp/slipstream.sh -d $slipstream_backend -e community -k local
+YUM_REPO_KIND=local
+YUM_REPO_EDITION=community
+
+declare -A YUM_REPO_TO_GH_BRANCH
+YUM_REPO_TO_GH_BRANCH[local]=master
+YUM_REPO_TO_GH_BRANCH[snapshot]=master
+YUM_REPO_TO_GH_BRANCH[candidate]=candidate-latest
+YUM_REPO_TO_GH_BRANCH[release]=release-latest
+
+_GH_PROJECT_URL=https://raw.githubusercontent.com/slipstream/SlipStream
+_GH_SCRIPTS_URL=$_GH_PROJECT_URL/${YUM_REPO_TO_GH_BRANCH[${YUM_REPO_KIND}]}/install
+_SS_PARAM_BACKEND="-d $slipstream_backend"
+if ( _is_true $with_refconf ); then
+    ss-set statecustom "Installing SlipStream WITH reference configuration..."
+    _NEXUS_URI=http://nexus.sixsq.com/service/local/artifact/maven/redirect
+    curl -k -sSfL \
+        -o /tmp/ss-install-ref-conf.sh \
+        $_GH_SCRIPTS_URL/ss-install-ref-conf.sh
+    chmod +x /tmp/ss-install-ref-conf.sh
+    /tmp/ss-install-ref-conf.sh \
+        -r $_NEXUS_URI'?r=snapshots-enterprise&g=com.sixsq.slipstream&a=SlipStreamReferenceConfiguration-'$REFCONF_NAME'-tar&p=tar.gz&c=bundle&v=LATEST' \
+        -u $NEXUS_CREDS \
+        -k $YUM_REPO_KIND \
+        -e $YUM_REPO_EDITION \
+        -o "$_SS_PARAM_BACKEND"
+
+    # Get and publish configured users.
+    _USER_PASS=$(
+    for user in /etc/slipstream/passwords/*; do
+        echo -n $(basename $user):$(cat $user),;
+    done)
+    _USER_PASS=${_USER_PASS%,}
+    ss-set ss_users $_USER_PASS
+
+    ### Get and publish connectors to test.
+    declare -A CONNECTORS
+    CONNECTORS=(
+        ["nuv.la"]='exoscale-ch-gva ultimum-cz1 ec2-eu-west nuvlabox-james-chadwick'
+        ["bb1.hnx.helix-nebula.eu"]='atos-es1 deac-lv1 ultimum-cz1'
+        ["connectors.community"]='atos-es1')
+
+    ss-set connectors_to_test "${CONNECTORS[$REFCONF_NAME]}"
+else
+    ss-set statecustom "Installing SlipStream WITHOUT reference configuration..."
+    export SLIPSTREAM_EXAMPLES=${install_examples}
+    curl -k -sSfL \
+        -o /tmp/slipstream.sh \
+        $_GH_SCRIPTS_URL/slipstream.sh
+    chmod +x /tmp/slipstream.sh
+    /tmp/slipstream.sh $_SS_PARAM_BACKEND -e $YUM_REPO_EDITION -k $YUM_REPO_KIND
+fi
 
 #
 # restarting services (probably not necessary)
@@ -137,35 +211,66 @@ service nginx restart
 #
 ss-set statecustom "SlipStream Ready!"
 hostname=`ss-get hostname`
-url="https://${hostname}"
-ss-set ss:url.service ${url}
+ss_url="https://${hostname}"
+ss-set ss:url.service ${ss_url}
 
 #
 # validate that the installation worked
 #
 ss-set statecustom "Validating service..."
+
+SS_UNAME=super
+SS_UPASS=supeRsupeR
+if [ -f /etc/slipstream/passwords/$SS_UNAME ]; then
+    SS_UPASS=$(cat /etc/slipstream/passwords/$SS_UNAME)
+fi
+profile_url="${ss_url}/user/$SS_UNAME"
+
 exit_code=0
-
-profile_url="${url}/user/super"
-
 tries=0
 while [ $tries -lt 5 ]; do
 
-  rc=`curl -k -s -u super:supeRsupeR -o /dev/null -w "%{http_code}" ${profile_url}`
-  echo "Return code from super profile page is " ${rc}
+  rc=`curl -k -s -u $SS_UNAME:$SS_UPASS -o /dev/null -w "%{http_code}" ${profile_url}`
+  echo "Return code from $SS_UNAME profile page is " ${rc}
   if [ "${rc}" -ne "200" ]; then
-    echo "Return code from super profile page was not 200."
+    echo "Return code from $SS_UNAME profile page was not 200."
     exit_code=1
   else
-    echo "Return code from super profile page was 200."
+    echo "Return code from $SS_UNAME profile page was 200."
     exit_code=0
-    exit $exit_code
+    break
   fi
 
   sleep 10
   tries=$[$tries+1]
 
 done
+# the service failed the validation
+if [ "$exit_code" -ne "0" ]; then
+   exit $exit_code
+fi
 
-exit $exit_code
+#
+# test the service
+#
 
+if ( _is_true $TEST_SERVICE ); then
+    CONNECTORS_TO_TEST=`ss-get connectors_to_test`
+    msg="Running deployment tests for connectors: '${CONNECTORS_TO_TEST}'"
+    ss-display "$msg"
+    echo $msg
+    USER=test
+    for CONNECTOR in ${CONNECTORS_TO_TEST}; do
+        msg="Running test on $CONNECTOR as $USER. $ss_url"
+        ss-display "$msg"
+        echo $msg
+        ss-execute -v -u $USER -p $(cat /etc/slipstream/passwords/$USER) --endpoint=$ss_url \
+            -w 15 --kill-vms-on-error \
+            --parameters "testclient:cloudservice=$CONNECTOR,apache:cloudservice=$CONNECTOR" \
+            examples/tutorials/service-testing/system | tee /tmp/ss-execute-$CONNECTOR.log
+    done
+else
+    msg="Skipped running deployment tests. $ss_url"
+    ss-display "$msg"
+    echo $msg
+fi
