efb2cb54cfe43fe0b18a5e71acb745a94ba0715d
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2015-12-14 13:52:58 +0100

    Updated to the latest UMD and rOCCI versions

--- install/connectors/connector-occi.sh
+++ install/connectors/connector-occi.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+rOCCI_VERSION=4.2.x
+UMD_VERSION=3.0.1-1
+
 function deploy() {
     yum install -y yum-priorities libxslt libxml2 libyaml dos2unix
-    rpm -ivH http://repository.egi.eu/sw/production/umd/2/sl6/x86_64/updates/umd-release-2.0.0-2.el6.noarch.rpm
+    rpm -ivH http://repository.egi.eu/sw/production/umd/${UMD_VERSION%%.*}/sl6/x86_64/updates/umd-release-${UMD_VERSION}.el6.noarch.rpm
 
     #
     # The link to the ROCCI repository at EGI is unstable.  In addition,
     # the actual RPM repository sometimes returns a 404 error, so working
     # around the instability of the repository isn't reliable.
     #
-    curl -o /etc/yum.repos.d/rocci.repo http://repository.egi.eu/community/software/rocci.cli/4.2.x/releases/repofiles/sl-6-x86_64.repo
+    curl -o /etc/yum.repos.d/rocci.repo http://repository.egi.eu/community/software/rocci.cli/${rOCCI_VERSION}/releases/repofiles/sl-6-x86_64.repo
 
     yum -y install slipstream-connector-occi
 }
