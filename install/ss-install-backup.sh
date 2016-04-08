2c172b693eb3d377b0f255d552a1781974068527
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2016-04-08 14:25:08 +0200

    add slipstream-backup-timestamp to the configuration

--- install/ss-install-backup.sh
+++ install/ss-install-backup.sh
@@ -56,6 +56,9 @@ function _configure() {
     sed -i -e "s|AMAZON_BUCKET=.*|AMAZON_BUCKET=${AMAZON_BUCKET}|" \
         -e "s|SS_HOSTNAME=.*|SS_HOSTNAME=${INSTANCE_ID}|" \
         /etc/slipstream/slipstream-backup.conf
+    mkdir -p /var/log/slipstream/
+    chown slipstream. /var/log/slipstream/
+    echo "BACKUP_TIMESTAMP=/var/log/slipstream/slipstream-backup-timestamp" >> /etc/slipstream/slipstream-backup.conf
     _prints "done."
 }
 
