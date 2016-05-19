3cb54d00b5867b9ef301ce6cbfe03f20a27a48a0
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2016-05-19 16:37:54 +0200

    updated to new locations of SS riemann stuff

--- install/slipstream.sh
+++ install/slipstream.sh
@@ -201,9 +201,9 @@ POSTGRESQL_DBS="slipstream ssclj"
 
 # Riemann variables.
 RIEMANN_VER=0.2.11-1
-ss_clj_client=/opt/slipstream/downloads/clj-ss-client.jar
+ss_clj_client=/opt/slipstream/riemann/lib/SlipStreamServiceOfferAPI.jar
 ss_riemann_conf=/etc/riemann/riemann-slipstream.config
-ss_riemann_streams=/opt/slipstream/riemann
+ss_riemann_streams=/opt/slipstream/riemann/streams
 
 # # # # # # # # # # # #
 # Advanced parameters.
@@ -668,7 +668,6 @@ function _install_riemann() {
 }
 
 function _add_ss_riemann_streams() {
-    mkdir -p $ss_riemann_streams
     cat > $ss_riemann_conf<<EOF
 ; -*- mode: clojure; -*-
 ; vim: filetype=clojure
@@ -689,16 +688,6 @@ EOF
 EXTRA_CLASSPATH=$ss_clj_client
 RIEMANN_CONFIG=$ss_riemann_conf
 EOF
-
-    # Hack.  Our Clojure `superstring` requires different version of java/lang/String.
-    #sed -i -e \
-    #   's|JAR="/usr/lib/riemann/riemann.jar:$EXTRA_CLASSPATH"|JAR="$EXTRA_CLASSPATH:/usr/lib/riemann/riemann.jar"|' \
-    #   /usr/bin/riemann
-    #
-    # NB! Damn... Riemann fails to start if we swap the classpath:
-    #java -cp /opt/slipstream/downloads/clj-ss-client.jar:/usr/lib/riemann/riemann.jar riemann.bin start /etc/riemann/riemann.config
-    #Exception in thread "main" java.lang.NoSuchMethodError: clojure.lang.Util.loadWithClass(Ljava/lang/String;Ljava/lang/Class;)Ljava/lang/Object;
-    #        at riemann.bin.<clinit>(Unknown Source)
 }
 
 function deploy_riemann() {
