52a3edf80ceb8532c10209d1f48eacd9c609c5e8
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2016-05-27 10:45:52 +0200

    minor

--- install/ss-ci-pipeline/ss-tester_reports.sh
+++ install/ss-ci-pipeline/ss-tester_reports.sh
@@ -5,4 +5,5 @@ set -e
 
 mkdir -p $SLIPSTREAM_REPORT_DIR/ss-test-results
 
-cp -rp ~/SlipStreamTests/clojure/target/* $SLIPSTREAM_REPORT_DIR/ss-test-results
+[ -d ~/SlipStreamTests/clojure/target ] && \
+   cp -rp ~/SlipStreamTests/clojure/target/* $SLIPSTREAM_REPORT_DIR/ss-test-results
