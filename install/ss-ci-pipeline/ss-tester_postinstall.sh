41a69b3f3e9bf8be9e17aca1f05d5075dae015f4
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2016-06-01 00:40:12 +0200

    need oracle jdk due to problem with aleph

--- install/ss-ci-pipeline/ss-tester_postinstall.sh
+++ install/ss-ci-pipeline/ss-tester_postinstall.sh
@@ -3,6 +3,10 @@
 set -x
 set -e
 
+# Hack due to an issue in aleph via kvlt.
+yum remove -y java
+yum localinstall -y http://yum.sixsq.com/thirdparty/jdk-8u91-linux-x64.rpm
+
 curl -fsSL -o /usr/bin/boot \
    https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh
 chmod 755 /usr/bin/boot
