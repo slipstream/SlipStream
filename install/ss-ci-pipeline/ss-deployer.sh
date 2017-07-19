232fbd465e7534c915c5e09bc9aaf0b3671ff224
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2017-07-19 12:06:36 +0200

    WIP: initial changes

--- install/ss-ci-pipeline/ss-deployer.sh
+++ install/ss-ci-pipeline/ss-deployer.sh
@@ -22,7 +22,7 @@
 set -x
 set -e
 
-function _is_true() {
+_is_true() {
     if [ "x${1}" == "xtrue" ]; then
         return 0
     else
@@ -30,7 +30,7 @@ function _is_true() {
     fi
 }
 
-function _is_none() {
+_is_none() {
     if [ "x${1}" == "xNone" ]; then
         return 0
     else
@@ -38,23 +38,43 @@ function _is_none() {
     fi
 }
 
-yum clean all
-yum upgrade -y
+_validate_deployment() {
+    ss-set statecustom "Validating deployed services..."
 
-ss-get --timeout 3600 builder.ready
+    SS_UNAME=super
+    SS_UPASS=supeRsupeR
+    if [ -f /etc/slipstream/passwords/$SS_UNAME ]; then
+        SS_UPASS=$(cat /etc/slipstream/passwords/$SS_UNAME)
+    fi
+    profile_url="${ss_url}/user/$SS_UNAME"
 
-install_examples=`ss-get install_examples`
-slipstream_backend=`ss-get slipstream_backend`
-with_refconf=`ss-get with_refconf`
-NEXUS_CREDS=`ss-get nexus_creds`
-REFCONF_NAME=`ss-get refconf_name`
-YUM_REPO_KIND=`ss-get yum_repo_kind`
-YUM_REPO_EDITION=`ss-get yum_repo_edition`
-SS_REPO_CONF_URL=`ss-get ss-repo-conf-url`
-install_scripts_branch=`ss-get install_scripts_branch`
-ES_HOST_PORT=`ss-get es-host-port`
+    exit_code=0
+    tries=0
+    while [ $tries -lt 5 ]; do
 
-function _install_yum_client_cert() {
+      rc=`curl -k -s -u $SS_UNAME:$SS_UPASS -o /dev/null -w "%{http_code}" ${profile_url}`
+      echo "Return code from $SS_UNAME profile page is " ${rc}
+      if [ "${rc}" -ne "200" ]; then
+        echo "Return code from $SS_UNAME profile page was not 200."
+        exit_code=1
+      else
+        echo "Return code from $SS_UNAME profile page was 200."
+        exit_code=0
+        break
+      fi
+
+      sleep 10
+      tries=$[$tries+1]
+
+    done
+    # the service failed the validation
+    if [ "$exit_code" -ne "0" ]; then
+       ss-set statecustom "ERROR: Service failed validation."
+       exit $exit_code
+    fi
+}
+
+_install_yum_client_cert() {
     SS_CONF_DIR=/etc/slipstream
     mkdir -p $SS_CONF_DIR
 
@@ -69,30 +89,19 @@ function _install_yum_client_cert() {
     rm -f $TARBALL
 }
 
-#
-# for installation from local repository
-#
+_gh_scripts_url() {
+    if [ "$install_scripts_branch" != "master" ]; then
+        branch=$install_scripts_branch
+    else
+        branch=${YUM_REPO_TO_GH_BRANCH[${YUM_REPO_KIND}]}
+    fi
+    echo $_GH_PROJECT_URL/$branch/install
+}
 
-declare -A YUM_REPO_TO_GH_BRANCH
-YUM_REPO_TO_GH_BRANCH[local]=master
-YUM_REPO_TO_GH_BRANCH[snapshot]=master
-YUM_REPO_TO_GH_BRANCH[candidate]=candidate-latest
-YUM_REPO_TO_GH_BRANCH[release]=release-latest
-
-_GH_PROJECT_URL=https://raw.githubusercontent.com/slipstream/SlipStream
-if [ "$install_scripts_branch" != "master" ]; then
-    branch=$install_scripts_branch
-else
-    branch=${YUM_REPO_TO_GH_BRANCH[${YUM_REPO_KIND}]}
-fi
-_GH_SCRIPTS_URL=$_GH_PROJECT_URL/$branch/install
-_SS_PARAM_BACKEND="-d $slipstream_backend"
-_NEXUS_URI=http://nexus.sixsq.com/service/local/artifact/maven/redirect
-if ( _is_true $with_refconf ); then
+_install_with_refconf() {
     ss-set statecustom "Installing SlipStream WITH reference configuration..."
-    curl -k -sSfL \
-        -o /tmp/ss-install-ref-conf.sh \
-        $_GH_SCRIPTS_URL/ss-install-ref-conf.sh
+    curl -k -sSfL -o /tmp/ss-install-ref-conf.sh \
+        $(_gh_scripts_url)/ss-install-ref-conf.sh
     chmod +x /tmp/ss-install-ref-conf.sh
     REF_CONF_URL=$_NEXUS_URI'?r=snapshots-enterprise-rhel7&g=com.sixsq.slipstream&a=SlipStreamReferenceConfiguration-'$REFCONF_NAME'-tar&p=tar.gz&c=bundle&v=LATEST'
     if ( _is_none ${SS_REPO_CONF_URL} ); then
@@ -130,29 +139,59 @@ if ( _is_true $with_refconf ); then
         ["connectors.community"]='ultimum-cz1')
 
     ss-set connectors_to_test "${CONNECTORS[$REFCONF_NAME]}"
-else
+}
+
+_install_without_refconf() {
     ss-set statecustom "Installing SlipStream WITHOUT reference configuration..."
     export SLIPSTREAM_EXAMPLES=${install_examples}
-    curl -k -sSfL \
-        -o /tmp/slipstream.sh \
-        $_GH_SCRIPTS_URL/slipstream.sh
+    curl -k -sSfL -o /tmp/slipstream.sh $(_gh_scripts_url)/slipstream.sh
     chmod +x /tmp/slipstream.sh
     if [ "X$YUM_REPO_EDITION" == "Xenterprise" ]; then
         _install_yum_client_cert \
             ${_NEXUS_URI}'?r=releases-enterprise&g=com.sixsq.slipstream&a=SlipStreamYUMCertsForSlipStreamInstaller&p=tgz&v=LATEST'
     fi
     if ( _is_none ${SS_REPO_CONF_URL} ); then
-        /tmp/slipstream.sh $_SS_PARAM_BACKEND -a $ES_HOST_PORT -e $YUM_REPO_EDITION -k $YUM_REPO_KIND
+        /tmp/slipstream.sh $_SS_PARAM_BACKEND -e $YUM_REPO_EDITION -k $YUM_REPO_KIND
     else
-        /tmp/slipstream.sh $_SS_PARAM_BACKEND -a $ES_HOST_PORT -x $SS_REPO_CONF_URL
+        /tmp/slipstream.sh $_SS_PARAM_BACKEND -x $SS_REPO_CONF_URL
     fi
-fi
+}
+
+
+yum clean all
+yum upgrade -y
+
+ss-get --timeout 3600 builder.ready
+
+install_examples=`ss-get install_examples`
+slipstream_backend=`ss-get slipstream_backend`
+with_refconf=`ss-get with_refconf`
+NEXUS_CREDS=`ss-get nexus_creds`
+REFCONF_NAME=`ss-get refconf_name`
+YUM_REPO_KIND=`ss-get yum_repo_kind`
+YUM_REPO_EDITION=`ss-get yum_repo_edition`
+SS_REPO_CONF_URL=`ss-get ss-repo-conf-url`
+install_scripts_branch=`ss-get install_scripts_branch`
 
 #
-# restarting services (probably not necessary)
-systemctl restart slipstream
-systemctl restart ssclj
-systemctl restart nginx
+# for installation from local repository
+#
+
+declare -A YUM_REPO_TO_GH_BRANCH
+YUM_REPO_TO_GH_BRANCH[local]=master
+YUM_REPO_TO_GH_BRANCH[snapshot]=master
+YUM_REPO_TO_GH_BRANCH[candidate]=candidate-latest
+YUM_REPO_TO_GH_BRANCH[release]=release-latest
+
+_GH_PROJECT_URL=https://raw.githubusercontent.com/slipstream/SlipStream
+_SS_PARAM_BACKEND="-d $slipstream_backend"
+_NEXUS_URI=http://nexus.sixsq.com/service/local/artifact/maven/redirect
+
+if ( _is_true $with_refconf ); then
+    _install_with_refconf
+else
+    _install_without_refconf
+fi
 
 #
 # set the service URL
@@ -163,42 +202,7 @@ ss_url="https://${hostname}"
 ss-set ss:url.service ${ss_url}
 ss-set ss_service_url ${ss_url}
 
-#
-# validate that the installation worked
-#
-ss-set statecustom "Validating service..."
-
-SS_UNAME=super
-SS_UPASS=supeRsupeR
-if [ -f /etc/slipstream/passwords/$SS_UNAME ]; then
-    SS_UPASS=$(cat /etc/slipstream/passwords/$SS_UNAME)
-fi
-profile_url="${ss_url}/user/$SS_UNAME"
-
-exit_code=0
-tries=0
-while [ $tries -lt 5 ]; do
-
-  rc=`curl -k -s -u $SS_UNAME:$SS_UPASS -o /dev/null -w "%{http_code}" ${profile_url}`
-  echo "Return code from $SS_UNAME profile page is " ${rc}
-  if [ "${rc}" -ne "200" ]; then
-    echo "Return code from $SS_UNAME profile page was not 200."
-    exit_code=1
-  else
-    echo "Return code from $SS_UNAME profile page was 200."
-    exit_code=0
-    break
-  fi
-
-  sleep 10
-  tries=$[$tries+1]
-
-done
-# the service failed the validation
-if [ "$exit_code" -ne "0" ]; then
-   ss-set statecustom "ERROR: Service failed validation."
-   exit $exit_code
-fi
+_validate_deployment
 
 ss-set statecustom "Service deployed and validated."
 ss-set ready true
